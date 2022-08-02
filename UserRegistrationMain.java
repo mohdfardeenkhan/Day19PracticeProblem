0c227affde26bf3494284298140c264949199a93
Mohd Fardeen Khan <96385727+mohdfardeenkhan@users.noreply.github.com>
2022-08-02 22:12:26 +0530

    uc10main

---
+++ UserRegistrationMain.java
@@ -0,0 +1,33 @@
+package com.bridgelabz;
+import java.util.regex.Pattern;
+public class UserRegistrationMain
+{
+    private static final String NAME = "^[A-Z]{1}[a-z]{2,}$";
+    private static final String EMAIL = "^(abc)[.][a-z]+[@](bl)[.](co)[.][a-z]+$";
+    private static final String PHONE_NUMBER = "^[\\d]{2}[\\s][\\d]{10}$";
+    private static final String PASSWORD = "^(?=.*[A-Z])(?=.*[a-z])(?=[^!@#$%^&+=]*[!@#$%^&+=][^!@#$%^&+=]*$)(?=.*[0-9]).{8,}$";
+
+    public boolean firstNameValidate(String fName)
+    {
+        return Pattern.matches(NAME, fName);
+    }
+    public boolean lastNameValidate(String lName)
+    {
+        return Pattern.matches(NAME, lName);
+    }
+
+    public boolean emailValidate(String email)
+    {
+        return Pattern.matches(EMAIL, email);
+    }
+
+    public boolean phoneNumberValidate(String phNum)
+    {
+        return Pattern.matches(PHONE_NUMBER, phNum);
+    }
+
+    public boolean passwordValidate(String pw)
+    {
+        return Pattern.matches(PASSWORD, pw);
+    }
+}
